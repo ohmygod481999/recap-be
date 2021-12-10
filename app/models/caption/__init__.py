3e348d28dad834afb39bc996eb5df27be5e0bd50
longvb <vuongbaolong48@gmail.com>
2021-12-10 21:50:17 +0700

    edit schema

--- app/models/caption/__init__.py
+++ app/models/caption/__init__.py
@@ -8,13 +8,15 @@ import datetime
 class Caption(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     content = db.Column(db.Text())
-    author_id = db.Column(UUID(as_uuid=True))
+    author_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'))
     created_at = db.Column(db.DateTime, default=datetime.datetime.now())
     status = db.Column(db.Integer)
 
-    def __init__(self, content, status):
+    def __init__(self, content, status, author_id, created_at):
         self.content = content
         self.status = status
+        self.author_id = author_id
+        self.created_at = created_at
 
 class CaptionSchema(ma.Schema):
     class Meta:
